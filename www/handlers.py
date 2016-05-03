a4d8332543a7301e1433f851dd1b65f2baa07b79
pcer <pengcong.chen@outlook.com>
2016-05-03 09:32:21 -0700

    finish first beautiful page by use uikit

--- www/handlers.py
+++ www/handlers.py
@@ -14,8 +14,14 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'hello, i am pcer'
+    blogs = [
+        Blog(id = '1', name = 'Test Blog', summary = summary, created_at = time.time() - 120),
+        Blog(id = '2', name = 'Something New', summary = summary, created_at = time.time() - 3600),
+        Blog(id = '3', name = 'Learn Swift', summary = summary, created_at = time.time() - 7200)
+
+    ]
     return {
-        '__template__' : 'test.html',
-        'users' : users
+        '__template__' : 'blogs.html',
+        'blogs' : blogs
     }
